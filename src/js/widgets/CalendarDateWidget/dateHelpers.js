210b29ba8d64746c42b982b9f657a766095d9cb5
Niicck <niicck@users.noreply.github.com>
2019-07-16 14:26:58 -0500

    add option to allow future dates in CalendarDatePicker

--- src/js/widgets/CalendarDateWidget/dateHelpers.js
+++ src/js/widgets/CalendarDateWidget/dateHelpers.js
@@ -1,5 +1,5 @@
 import { set } from 'lodash';
-import { isAfter } from 'date-fns';
+import { isAfter, isBefore } from 'date-fns';
 
 // dateString will be either a string in the form '2011-11-11' or undefined
 export function parseISODate(dateString) {
@@ -47,7 +47,7 @@ export function formatISOPartialDate({ month, day, year }) {
 // Make sure that date is valid. If invalid, plug in approximate date.
 // Approximation is made by js Date parser's convention.
 // Ex: '02-31-2019' => '03-31-2019'
-export function setValidDate(newState) {
+export function setValidDate(newState, validation) {
   const { year, month, day } = newState.value;
   // A reminder that js Dates start months at index=0 ;)
   const validDate = new Date(year, month - 1, day);
@@ -62,16 +62,57 @@ export function setValidDate(newState) {
     set(newState, ['value', 'day'], formatDayMonth(validDay));
   }
 
-  // If date is in the future, go back to last year
-  const isDateInFuture = isAfter(
-    new Date(year, newState.value.month - 1, newState.value.day),
-    new Date()
-  );
-  if (isDateInFuture) {
-    // Only need to subtract 1 year,
-    // Because we do not allow years that are greater than the current year
-    set(newState, ['value', 'year'], year - 1);
+  if (validation === 'pastOnly') {
+    // If date is in the future, go back to last year
+    const isDateInFuture = isAfter(
+      new Date(year, newState.value.month - 1, newState.value.day),
+      new Date()
+    );
+    if (isDateInFuture) {
+      // Only need to subtract 1 year,
+      // Because we do not allow years that are greater than the current year
+      set(newState, ['value', 'year'], year - 1);
+    }
+  } else if (validation === 'futureOnly') {
+    // Note: autocorrect won't work the same for future dates,
+    // unless we decided to start tracking "focus" on the year input box.
   }
 
   return newState;
+}
+
+/**
+  For validation="pastOnly"
+  If the filled out month+day for this year are in the future, then set year to last year.
+  ex:
+    Today is 5/07/2019
+    newState = {value: {month: "11", day: "1"}}
+    11/01/2019 is after 05/07/2019, so its not valid for a "pastOnly" calendar.
+    So we set the year to 2018
+*/
+export function autofillYearPast(newState) {
+  const currentYear = (new Date()).getFullYear();
+  const isDateFromLastYear = isAfter(
+    new Date(currentYear, newState.value.month - 1, newState.value.day),
+    new Date()
+  );
+  return isDateFromLastYear ? currentYear - 1 : currentYear;
+}
+
+/**
+  For validation="futureOnly"
+  If the filled out month+day for this year are in the past, then set year to next year.
+  ex:
+    Today is 5/07/2019
+    newState = {value: {month: "03", day: "1"}}
+    03/01/2019 is before 05/07/2019, so its not valid for a "futureOnly" calendar.
+    So we set the year to 2020
+* */
+export function autofillYearFuture(newState) {
+  const currentYear = (new Date()).getFullYear();
+  const isDateInPast = isBefore(
+    new Date(currentYear, newState.value.month - 1, newState.value.day),
+    new Date()
+  );
+  return isDateInPast ? currentYear + 1 : currentYear;
 }
--- src/js/widgets/CalendarDateWidget/index.jsx
+++ src/js/widgets/CalendarDateWidget/index.jsx
@@ -1,186 +1,221 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import Flatpickr from 'react-flatpickr';
-import { isAfter } from 'date-fns';
 import { set, cloneDeep } from 'lodash';
+import { isAfter, isBefore } from 'date-fns';
 
-import {formatYear, formatDayMonth, parseISODate, formatISOPartialDate, setValidDate} from './dateHelpers';
+import {formatYear, formatDayMonth, parseISODate, formatISOPartialDate, setValidDate, autofillYearPast, autofillYearFuture} from './dateHelpers';
 
-// Returns datestring in the form '2011-11-22'
-// Uses parts of us-form-system's DateWidget. Includes a Calendar date picker.
-// ref: https://github.com/cityofaustin/us-forms-system/blob/master/src/js/widgets/DateWidget.jsx
-export default class CalendarDateWidget extends React.Component {
-  constructor(props) {
-    super(props);
+/**
+  Returns datestring in the form '2011-11-22'
+  Uses parts of us-form-system's DateWidget. Includes a Calendar date picker.
+  ref: https://github.com/cityofaustin/us-forms-system/blob/master/src/js/widgets/DateWidget.jsx
 
-    this.state = {
-      value: parseISODate(this.props.value),
-      touched: {
-        month: false,
-        day: false,
-        year: false,
-      },
-    };
-    this.onChange = this.onChange.bind(this);
-    this.handleFormChange = this.handleFormChange.bind(this);
-    this.handleFlatpickrChange = this.handleFlatpickrChange.bind(this);
-  }
+  @param options.validation:
+    "pastOnly": only allow dates set in the past
+    "futureOnly": only allow dates set in the future
+**/
+const CalendarDateWidget = (options) => {
+  return class CalendarDateWidget extends Component {
 
-  onChange({ dates, datestring }) {
-    this.props.onChange(datestring);
-  }
+    constructor(props) {
+      super(props);
 
-  isIncomplete({ month, day, year }) {
-    return (!month || !day || !year)
-  }
+      this.state = {
+        value: parseISODate(this.props.value),
+        touched: {
+          month: false,
+          day: false,
+          year: false,
+        },
+      };
+      this.onChange = this.onChange.bind(this);
+      this.handleFormChange = this.handleFormChange.bind(this);
+      this.handleFlatpickrChange = this.handleFlatpickrChange.bind(this);
 
-  handleFormChange(field, value) {
-    const currentYear = (new Date()).getFullYear();
-
-    if (field === 'month') {
-      if ((value > 12)) {
-        return
-      }
-      if (value < 1) {
-        value = '';
-      }
-    }
-    if (field === 'day') {
-      if (value > 31) {
-        return
-      }
-      if (value < 1) {
-        value = '';
-      }
-    }
-    if (field === 'year') {
-      if (value > currentYear) {
-        value = currentYear;
-      }
-      if (value < 1) {
-        value = '';
-      }
+      this.validation = options.validation;
     }
 
-    let newState = cloneDeep(this.state);
-    const formatter = (field === 'year') ? formatYear : formatDayMonth;
-
-    set(newState, ['value', field], formatter(value));
-    set(newState, ['touched', field], true);
-
-    // Autofill year after filling month and day
-    if (
-      (
-        ((field === 'day') && (!!this.state.value.month)) ||
-        ((field === 'month') && (!!this.state.value.day))
-      ) && (!this.state.value.year)
-    ) {
-      const isDateFromLastYear = isAfter(
-        new Date(currentYear, newState.value.month - 1, newState.value.day),
-        new Date(new Date())
-      );
-      const autofillYear = isDateFromLastYear ? currentYear - 1 : currentYear;
-      set(newState, ['value', 'year'], autofillYear);
+    onChange({ dates, datestring }) {
+      this.props.onChange(datestring);
     }
 
-    // If all 3 values are filled out, make sure they are a valid day
-    if (
-      !!newState.value.year &&
-      !!newState.value.day &&
-      !!newState.value.month
-    ) {
-      newState = setValidDate(newState);
-    }
-
-    this.setState(newState, () => {
-      if (this.isIncomplete(newState.value)) {
-        this.props.onChange();
+    isIncomplete({ month, day, year }) {
+      if (!month || !day || !year) {
+        return true
+      } else if (
+        (this.validation === 'pastOnly') &&
+        (isAfter(new Date(year, month - 1, day), new Date()))
+      ) {
+        return true
+      } else if (
+        (this.validation === 'futureOnly') &&
+        (isBefore(new Date(year, month - 1, day), new Date()))
+      ) {
+        return true
       } else {
-        this.props.onChange(formatISOPartialDate(newState.value));
+        return false
       }
-    });
-  }
+    }
 
-  handleFlatpickrChange(dateString) {
-    let newState = cloneDeep(this.state);
-    set(newState, 'value', parseISODate(dateString));
-    set(newState, 'touched', {month: true, day: true, year: true});
+    handleFormChange(field, value) {
+      const currentYear = (new Date()).getFullYear();
 
-    this.setState(newState, () => {
-      if (this.isIncomplete(newState.value)) {
-        this.props.onChange();
-      } else {
-        this.props.onChange(formatISOPartialDate(newState.value));
+      if (field === 'month') {
+        if ((value > 12)) {
+          return
+        }
+        if (value < 1) {
+          value = '';
+        }
       }
-    })
-  }
+      if (field === 'day') {
+        if (value > 31) {
+          return
+        }
+        if (value < 1) {
+          value = '';
+        }
+      }
+      if (field === 'year') {
+        if ((this.validation == "pastOnly") && (value > currentYear)) {
+          value = currentYear;
+        } else if ((this.validation == "futureOnly") && (value < currentYear)) {
+          // value = currentYear;
+        }
+        if (value < 1) {
+          value = '';
+        }
+      }
 
-  render() {
-    const {id} = this.props;
-    const {month, day, year} = this.state.value;
-    const flatpickrDate = (month && day && year) ?
-      formatISOPartialDate({month,day,year}) : undefined;
+      let newState = cloneDeep(this.state);
+      const formatter = (field === 'year') ? formatYear : formatDayMonth;
 
-    //<span> state date: {formatISOPartialDate({month,day,year})} </span><br/>
-    return (
-      <div className="date-widget-container">
+      set(newState, ['value', field], formatter(value));
+      set(newState, ['touched', field], true);
 
-        <div className="date-widget-values-container">
-          <fieldset className="date-fieldset" id={id}>
-            <div className="usa-date-of-birth">
-              <div className="usa-datefield usa-form-group usa-form-group-month">
-                <label className="input-date-label smaller-input" htmlFor={`${id}_Month`}>Month</label>
-                <input
-                  className="usa-input-inline" id={`${id}_Month`} name={`${id}_Month`}
-                  type="number" min="1" max="12" value={month}
-                  onChange={(event) => this.handleFormChange('month', event.target.value)}
-                />
+      // Autofill year after filling month and day
+      if (
+        (
+          ((field === 'day') && (!!this.state.value.month)) ||
+          ((field === 'month') && (!!this.state.value.day))
+        ) && (!this.state.value.year)
+      ) {
+        let autofillYear = currentYear;
+        if (this.validation == "pastOnly") {
+          autofillYear = autofillYearPast(newState)
+        } else if (this.validation == "futureOnly") {
+          autofillYear = autofillYearFuture(newState)
+        }
+        set(newState, ['value', 'year'], autofillYear);
+      }
+
+      // If all 3 values are filled out, make sure they are a valid day
+      if (
+        !!newState.value.year &&
+        !!newState.value.day &&
+        !!newState.value.month
+      ) {
+        newState = setValidDate(newState, this.validation);
+      }
+
+      this.setState(newState, () => {
+        if (this.isIncomplete(newState.value)) {
+          this.props.onChange();
+        } else {
+          this.props.onChange(formatISOPartialDate(newState.value));
+        }
+      });
+    }
+
+    handleFlatpickrChange(dateString) {
+      let newState = cloneDeep(this.state);
+      set(newState, 'value', parseISODate(dateString));
+      set(newState, 'touched', {month: true, day: true, year: true});
+
+      this.setState(newState, () => {
+        if (this.isIncomplete(newState.value)) {
+          this.props.onChange();
+        } else {
+          this.props.onChange(formatISOPartialDate(newState.value));
+        }
+      })
+    }
+
+    render() {
+      const {id} = this.props;
+      const {month, day, year} = this.state.value;
+      const flatpickrDate = (month && day && year) ?
+        formatISOPartialDate({month,day,year}) : undefined;
+
+      const flatpickrOptions = {
+        dateFormat: 'Y-m-d',
+        wrap: true,
+        allowInput: false,
+        enableTime: false,
+      }
+      if (this.validation == "pastOnly") {
+        flatpickrOptions.maxDate = 'today';
+      } else if (this.validation == "futureOnly") {
+        flatpickrOptions.minDate = 'today';
+      }
+
+      return (
+        <div className="date-widget-container">
+
+          <div className="date-widget-values-container">
+            <fieldset className="date-fieldset" id={id}>
+              <div className="usa-date-of-birth">
+                <div className="usa-datefield usa-form-group usa-form-group-month">
+                  <label className="input-date-label smaller-input" htmlFor={`${id}_Month`}>Month</label>
+                  <input
+                    className="usa-input-inline" id={`${id}_Month`} name={`${id}_Month`}
+                    type="number" min="1" max="12" value={month}
+                    onChange={(event) => this.handleFormChange('month', event.target.value)}
+                  />
+                </div>
+                <div className="usa-datefield usa-form-group usa-form-group-day">
+                  <label className="input-date-label smaller-input" htmlFor={`${id}_Day`}>Day</label>
+                  <input
+                    className="usa-input-inline" id={`${id}_Day`} name={`${id}_Day`}
+                    type="number" min="1" max="31" value={day}
+                    onChange={(event) => this.handleFormChange('day', event.target.value)}
+                  />
+                </div>
+                <div className="usa-datefield usa-form-group usa-form-group-year usa-form-group-year-fix">
+                  <label className="input-date-label smaller-input" htmlFor={`${id}_Year`}>Year</label>
+                  <input
+                    className="usa-input-inline" id={`${id}_Year`} name={`${id}_Year`}
+                    type="number" min="1900" value={year}
+                    onChange={(event) => this.handleFormChange('year', event.target.value)}
+                  />
+                </div>
+                <div aria-hidden className="usa-datefield usa-form-group flatpickr-container">
+                  <Flatpickr
+                    options={flatpickrOptions}
+                    value={flatpickrDate}
+                    onChange={(dates, datestring) => this.handleFlatpickrChange(datestring)}
+                  >
+                    <label className="hidden-content" htmlFor={`${id}_calendar`}>Calendar</label>
+                    <input className="hidden-flatpickr-input-box" tabIndex="-1" type="text" data-input name={`${id}_calendar`} id={`${id}_calendar`}/>
+                    <span className="usa-input-inline flatpickr-input-button-container" title="toggle" data-toggle>
+                      <i className="material-icons flatpickr-input-button">event</i>
+                    </span>
+                  </Flatpickr>
+                </div>
               </div>
-              <div className="usa-datefield usa-form-group usa-form-group-day">
-                <label className="input-date-label smaller-input" htmlFor={`${id}_Day`}>Day</label>
-                <input
-                  className="usa-input-inline" id={`${id}_Day`} name={`${id}_Day`}
-                  type="number" min="1" max="31" value={day}
-                  onChange={(event) => this.handleFormChange('day', event.target.value)}
-                />
-              </div>
-              <div className="usa-datefield usa-form-group usa-form-group-year usa-form-group-year-fix">
-                <label className="input-date-label smaller-input" htmlFor={`${id}_Year`}>Year</label>
-                <input
-                  className="usa-input-inline" id={`${id}_Year`} name={`${id}_Year`}
-                  type="number" min="1900" value={year}
-                  onChange={(event) => this.handleFormChange('year', event.target.value)}
-                />
-              </div>
-              <div aria-hidden className="usa-datefield usa-form-group flatpickr-container">
-                <Flatpickr
-                  options={{
-                    dateFormat: 'Y-m-d',
-                    maxDate: 'today',
-                    wrap: true,
-                    allowInput: false,
-                    enableTime: false,
-                  }}
-                  value={flatpickrDate}
-                  onChange={(dates, datestring) => this.handleFlatpickrChange(datestring)}
-                >
-                  <label className="hidden-content" htmlFor={`${id}_calendar`}>Calendar</label>
-                  <input className="hidden-flatpickr-input-box" tabIndex="-1" type="text" data-input name={`${id}_calendar`} id={`${id}_calendar`}/>
-                  <span className="usa-input-inline flatpickr-input-button-container" title="toggle" data-toggle>
-                    <i className="material-icons flatpickr-input-button">event</i>
-                  </span>
-                </Flatpickr>
-              </div>
-            </div>
-          </fieldset>
+            </fieldset>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
+
+  CalendarDateWidget.propTypes = {
+    id: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    value: PropTypes.string
+  };
 }
 
-CalendarDateWidget.propTypes = {
-  id: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  value: PropTypes.string
-};
+export default CalendarDateWidget;
